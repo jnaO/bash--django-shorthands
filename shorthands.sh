dd87121e52e4b23fd954cceeaffb7ba84fa4cd24
Jan Lofberg <chenebu@gmail.com>
2012-07-20 15:19:14 +0200

    initial commit, south merge added

---
+++ shorthands.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 2012 by jnaO
+#
+# Intension is to help you, not to harm you.
+#
+# That said, permission is hereby granted, free of charge and so on...
+# Use as you please. If you break something using tis program, or
+# if anyone else breaks something for you because of this program
+# don't blame me.
+#
+# Source is found @ https://github.com/jnaO/file_backup
+#
+if [ $1 == 'migrate' ] || [ $1 == '-m' ]
+  then
+    echo "================================================"
+    echo "Creating schema migration for "$2
+    echo "================================================"
+    python manage.py schemamigration $2 --auto
+    echo "================================================"
+    echo "Migrating "$2
+    echo "================================================"
+    python manage.py migrate $2
+fi
